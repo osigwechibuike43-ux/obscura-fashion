c80d9be9b21c2e4335f6bf6df80af40c07c38b4d
osigwechibuike43-ux <osigwechibuike43@gmail.com>
2026-05-25 16:16:42 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// A simple function to simulate adding an item to the cart
+function addToCart() {
+    const size = document.getElementById('size').value;
+    alert(`Item added to cart! Size selected: ${size}`);
+    
+    // In a real application, you would save this data to localStorage 
+    // or send it to a backend database here.
+}
